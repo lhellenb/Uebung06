abbcfab05a0e86fdbfaf5a5943fab43612aee183
unknown <laura.hellenbroich@netcologne.de>
2025-11-27 14:46:58 +0100

    p1

---
+++ src/p1/P1_main.java
@@ -0,0 +1,11 @@
+package p1;
+
+public class P1_main {
+	public static void main(String[] args) {
+		int x=9;
+		squared(x);
+		System.out.println(squared(x)); }
+public static int squared(int i) {
+	return (i+1) * (i+1);
+}
+}
